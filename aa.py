bf3267ff4e871de799e0e398586c20252c95e51b
Guilherme Patric <94064499+GuilhermePatric@users.noreply.github.com>
2026-09-21 22:27:08 -0300

    erro funcao calc

--- aa.py
+++ aa.py
@@ -4,5 +4,4 @@ import pandas as pd
 def converter(inicio, fim):
     inicio = pd.to_datetime(inicio, format='%m_%Y')
     fim = pd.to_datetime(fim, format='%m_%Y')
-    return (fim - inicio).days+1
-
+    return (fim - inicio).days
